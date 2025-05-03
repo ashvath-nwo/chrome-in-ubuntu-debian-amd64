43391a8d5eb6a4a389faafa8bf93ab9d7c800e77
ashvath-nwo <mashaultrafunkthecoder@gmail.com>
2025-05-03 16:52:43 +0530

    Create install_chrome_amd64.sh

---
+++ scripts/install_chrome_amd64.sh
@@ -0,0 +1,10 @@
+# Installing google chrome via APT (sadly no SNAP available from Google, like there is for Chromium)
+
+# Setup the Google signer and repo
+curl -O https://packages.cloud.google.com/apt/doc/apt-key.gpg && sudo apt-key add apt-key.gpg
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' 
+
+# Install
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+echo Chrome is installed. execute it by typing chrome in terminal.
